f332b7fa988f3e382abd3ed521ee5cba3f30ee24
tumlungaen <mr.kittinan@gmail.com>
2022-05-25 11:59:14 +0700

    complete test.py to machine learning, predict data

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from sklearn import tree
-features = [ [120, 5], [110, 5], [180, 12], [163, 18], [159, 13] ]
+features = [ [120, 5], [110, 5], [180, 12], [163, 18], [159, 13], [113, 52] ]
 labels = [ "ECO Car", "Normal Car", "BUS", "VAN", "Sport Car", "" ]
 classifier = tree.DecisionTreeClassifier()
 classifier = classifier.fit( features, labels )
 
-#print( classifier.predict[ [150, 12] ] )
+print( classifier.predict( [ [150, 12] ] ) )
