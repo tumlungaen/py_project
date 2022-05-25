200b6e0e5e4bff12ff9f85966740766264fa4b16
tumlungaen <mr.kittinan@gmail.com>
2022-05-25 11:56:24 +0700

    add test.py to server

---
+++ test.py
@@ -0,0 +1,7 @@
+from sklearn import tree
+features = [ [120, 5], [110, 5], [180, 12], [163, 18], [159, 13] ]
+labels = [ "ECO Car", "Normal Car", "BUS", "VAN", "Sport Car", "" ]
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit( features, labels )
+
+#print( classifier.predict[ [150, 12] ] )
